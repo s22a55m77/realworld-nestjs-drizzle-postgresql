3677ab445f719bf58ba30aa87f1259759042131f
s22a55m77 <s22a55m77@gmail.com>
2024-05-06 19:17:02 +0800

    Update CreateTagDTO to omit 'id' field in tag creation

--- src/module/tag/tag.dto.ts
+++ src/module/tag/tag.dto.ts
@@ -2,7 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { createZodDto } from 'nestjs-zod';
 import { insertTagSchema, selectTagSchema } from 'src/module/tag/tag.schema';
 
-export class CreateTagDTO extends createZodDto(insertTagSchema) {}
+export class CreateTagDTO extends createZodDto(
+  insertTagSchema.omit({ id: true }),
+) {}
 
 class Tag extends createZodDto(selectTagSchema) {}
 
